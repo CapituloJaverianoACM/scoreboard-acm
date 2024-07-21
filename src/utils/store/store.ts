46b89efe9bea3466ee9fba72587c5520140a495b
juanfra312003 <juanframireze@javeriana.edu.co>
2024-07-20 21:04:20 -0500

    Feature: Redux Problems

--- src/utils/store/store.ts
+++ src/utils/store/store.ts
@@ -1,32 +1,32 @@
-import {configureStore} from '@reduxjs/toolkit'
-import resultsSlice from "./resultsSlice";
-import {
-    createStateSyncMiddleware,
-    initMessageListener
-} from 'redux-state-sync'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { createStateSyncMiddleware, initMessageListener } from 'redux-state-sync';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import resultsReducer from './resultsSlice';
+import problemReducer from './problemSlice';
 
 const persistConfig = {
     key: 'root',
-    storage
-}
+    storage,
+};
 
-const persistedReducer = persistReducer(persistConfig, resultsSlice)
+const rootReducer = combineReducers({
+    results: resultsReducer,
+    problems: problemReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-    reducer: {
-        results: persistedReducer
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(createStateSyncMiddleware({
-            blacklist: ["persist/PERSIST", "persist/REHYDRATE"]
+            blacklist: ['persist/PERSIST', 'persist/REHYDRATE'],
         })),
-})
+});
 
-initMessageListener(store)
+initMessageListener(store);
 
-export const persistor = persistStore(store)
-
+export const persistor = persistStore(store);
 export default store;
