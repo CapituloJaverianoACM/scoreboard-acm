2c99ba2297eed53964dfad1168e67da0d8e1cf19
juanfra312003 <juanframireze@javeriana.edu.co>
2024-07-20 21:32:31 -0500

    Feature: Validation letter without message

--- src/components/contest/CreateContest.tsx
+++ src/components/contest/CreateContest.tsx
@@ -1,12 +1,14 @@
 import { ReactElement, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../utils/store/store"; // Asegúrate de exportar RootState desde store
 import { addProblem } from "../../utils/store/problemSlice";
 import { Problem } from "../../utils/types/contest";
+import {RootState} from "@reduxjs/toolkit/query";
 
 const CreateContest = (): ReactElement => {
     const dispatch = useDispatch();
-    const problems = useSelector((state: RootState) => state.problems.value); // Selecciona el estado de los problemas
+    const problems = useSelector((state: RootState) => {
+        return state.problems.value;
+    });
     const [problemLetter, setProblemLetter] = useState<string>("");
     const [problemName, setProblemName] = useState<string>("");
 
@@ -18,8 +20,15 @@ const CreateContest = (): ReactElement => {
         setProblemName(e.target.value);
     };
 
+    function validateProblemLetter(problemLetter: string): boolean {
+        if (problemLetter.length !== 1) {
+            return false;
+        }
+        return problemLetter.charCodeAt(0) - "A".charCodeAt(0) === problems.length;
+    }
+
     const handleSubmit = () => {
-        if (problemLetter.trim() && problemName.trim()) {
+        if (validateProblemLetter(problemLetter) && problemName.length > 0){
             const problem: Problem = {
                 letter: problemLetter,
                 name: problemName,
@@ -29,33 +38,14 @@ const CreateContest = (): ReactElement => {
             setProblemLetter("");
             setProblemName("");
         }
+        else{
+            console.log("Invalid problem letter or name");
+        }
     };
 
     return (
         <div className="relative h-[85vh] flex flex-col justify-evenly items-center text-white">
             <h1 className="text-4xl">Create Contest</h1>
-            <div className="flex flex-col items-center">
-                <input
-                    type="text"
-                    placeholder="Problem Letter"
-                    className="p-2 m-2"
-                    value={problemLetter}
-                    onChange={handleLetterChange}
-                />
-                <input
-                    type="text"
-                    placeholder="Problem Name"
-                    className="p-2 m-2"
-                    value={problemName}
-                    onChange={handleNameChange}
-                />
-                <button
-                    onClick={handleSubmit}
-                    className="p-2 bg-blue-500 rounded text-white"
-                >
-                    Add Problem
-                </button>
-            </div>
             <div className="mt-4">
                 <h2 className="text-2xl">Problems List</h2>
                 <ul>
@@ -66,6 +56,30 @@ const CreateContest = (): ReactElement => {
                     ))}
                 </ul>
             </div>
+            <div className="flex flex-col items-center">
+                <input
+                    type="text"
+                    placeholder="Problem Letter"
+                    className="p-2 m-2 bg-gray-200 text-black border border-gray-400 rounded"
+                    value={problemLetter}
+                    onChange={handleLetterChange}
+                />
+                <input
+                    type="text"
+                    color={"black"}
+                    placeholder="Problem Name"
+                    className="p-2 m-2 bg-gray-200 text-black border border-gray-400 rounded"
+                    value={problemName}
+                    onChange={handleNameChange}
+                />
+                <button
+                    onClick={handleSubmit}
+                    className="p-2 bg-blue-500 rounded text-white"
+                >
+                    Add Problem
+                </button>
+            </div>
+
         </div>
     );
 };
