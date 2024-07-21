c84a676534cadd7b3ad52940b1b4ad262152e718
TalkySafe143 <sebastian.galindo143@gmail.com>
2024-07-20 21:46:25 -0500

    Fix contributors

--- src/components/landing/Person.tsx
+++ src/components/landing/Person.tsx
@@ -2,9 +2,9 @@ import {ReactElement} from "react";
 
 const Person = ({ person }) : ReactElement => {
     return (
-        <a href={person.follow}>
+        <a className="p-10" href={person.follow}>
             <div className="flex justify-center flex-col items-center gap-5">
-                <img className="rounded-full max-h-40" src={person.picture} alt=""/>
+                <img className="rounded-full max-h-36" src={person.picture} alt=""/>
                 <h1>{person.name}</h1>
             </div>
         </a>
