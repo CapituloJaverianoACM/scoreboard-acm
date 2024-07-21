a1733598ceac8bcee6e1d3c75d329f1fac65f9c8
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-07-21 18:16:38 -0500

    Feat scoreboard style

--- src/components/scoreboard/ScoreBoard.tsx
+++ src/components/scoreboard/ScoreBoard.tsx
@@ -1,68 +1,43 @@
 import {ReactElement} from "react";
 import {useSelector} from "react-redux";
 import ScoreboardMessage from "../../utils/types/scoreboard.ts";
-import {Contest, TeamStatus} from "../../utils/types/contest.ts";
+import {Contest, Problem, TeamStatus} from "../../utils/types/contest.ts";
 import TeamRow from "./(team)/TeamRow.tsx";
 import {useLocation} from "react-router-dom";
 const ScoreBoard = () : ReactElement => {
 
-    const resultsList : ScoreboardMessage[] = useSelector(state => state.results.value);
+    const resultsList : ScoreboardMessage[] = useSelector((state : any) => state.results.value);
     // console.log(`Este es el tam ${resultsList.length}`)
     const location = useLocation();
     const contest = location.state?.contest;
     console.log(contest);
 
-    const exampleTeamStatus : TeamStatus = {
-        team: {
-            name: "Equipo 1",
-            shortName: "E1"
-        },
-        results: [
-            {
-                problem: {
-                    name: "Problema 1",
-                    letter: "A"
-                },
-                status: "AC",
-                tries: 1,
-                acceptedMinute: 10
-            },
-            {
-                problem: {
-                    name: "Problema 2",
-                    letter: "B"
-                },
-                status: "SOLVED",
-                tries: 2,
-                acceptedMinute: 0
-            },
-            {
-                problem: {
-                    name: "Problema 3",
-                    letter: "C"
-                },
-                status: "PENDING",
-                tries: 2,
-                acceptedMinute: 0
-            },
-            {
-                problem: {
-                    name: "Problema 4",
-                    letter: "D"
-                },
-                status: "WA",
-                tries: 2,
-                acceptedMinute: 0
-            }
-        ],
-        penalty: 0
-    }
+    const teams : TeamStatus[] = useSelector((state : any) => state.teamStatus.value);
+    const problems : Problem[] = useSelector((state : any) => state.problems.value);
 
     return (
-        <>
-            <h1>Tabla de posiciones</h1>
-            <TeamRow teamStatus={exampleTeamStatus} />
-        </>
+        <div className="p-50">
+            <h1 className="p-5 text-2xl">Tabla de posiciones</h1>
+            {/* Header row */}
+            <div className="flex flex-row space-x-4 text-[25px] ">
+                <div className="w-96 h-20 flex items-center justify-center text-center" style={{borderRadius: '5px'}}>
+                    Equipo
+                </div>
+                {problems.map((problem, index) => {
+                    return (
+                        <div key={index} className="w-20 h-20 flex items-center justify-center text-center" style={{borderRadius: '5px'}}>
+                            {problem.letter}
+                        </div>
+                    );
+                })}
+            </div>
+            {/* Teams */}
+            <div className="flex flex-col space-y-4">
+                {teams.map((teamStatus, index) => (
+                    <TeamRow key={index} pos={index+1} teamStatus={teamStatus} />
+                ))}
+            </div>
+        </div>
     );
 }
 
