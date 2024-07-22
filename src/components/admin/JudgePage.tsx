2311b445ba04d40e37eb2f4deb354773e4514ddb
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-07-22 17:11:09 -0500

    Advance on JudgePage layout

--- src/components/admin/JudgePage.tsx
+++ src/components/admin/JudgePage.tsx
@@ -2,9 +2,26 @@ import {ReactElement} from "react";
 
 const JudgePage = () : ReactElement => {
     return (
-        <>
-            <h1>Pagina para juzgar resultados</h1>
-        </>
+        <div className="text-white flex items-center justify-center h-[100vh] w-full px-20">
+            <div className="w-full h-full flex flex-col items-center justify-evenly">
+                <div>
+                    <p>Timer</p>
+                </div>
+                <div>
+                    <p>Recent Submissions</p>
+                </div>
+            </div>
+            <div className="w-full h-full flex flex-col items-center justify-evenly">
+                <div className="flex flex-col">
+                    <button>Iniciar contest</button>
+                    <button>Pausar contest</button>
+                    <button>Ir al revelator</button>
+                </div>
+                <div className="flex items-center justify-center">
+                    <button>Agregar veredicto</button>
+                </div>
+            </div>
+        </div>
     );
 }
 
