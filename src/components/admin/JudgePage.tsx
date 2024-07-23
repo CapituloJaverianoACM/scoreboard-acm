54f6349daefc0b5d03b61e7ac6acc2f20374fda5
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-07-23 15:31:51 -0500

    Advance on JudgePage and TImer

--- src/components/admin/JudgePage.tsx
+++ src/components/admin/JudgePage.tsx
@@ -1,24 +1,41 @@
 import {ReactElement} from "react";
+import { PlusIcon } from "@heroicons/react/20/solid";
+import Timer from "../scoreboard/Timer";
 
 const JudgePage = () : ReactElement => {
+    const time = new Date();
+    time.setSeconds(time.getSeconds() + 20);
     return (
         <div className="text-white flex items-center justify-center h-[100vh] w-full px-20">
-            <div className="w-full h-full flex flex-col items-center justify-evenly">
+            <div className="w-full h-full flex flex-col items-center pt-24 gap-12">
                 <div>
-                    <p>Timer</p>
+                    <Timer expiryTimestamp={time} />
                 </div>
-                <div>
+                <div className="flex-1">
                     <p>Recent Submissions</p>
                 </div>
             </div>
-            <div className="w-full h-full flex flex-col items-center justify-evenly">
-                <div className="flex flex-col">
-                    <button>Iniciar contest</button>
-                    <button>Pausar contest</button>
-                    <button>Ir al revelator</button>
+            <div className="w-full h-full flex flex-col items-center pt-24 gap-12">
+                <div className="flex flex-col gap-3 h-[35vh] justify-center">
+                    <button
+                        className="transition duration-500 w-[15vw] text-xl p-3 border-2 rounded-full hover:bg-white hover:text-black">
+                        Iniciar contest
+                    </button>
+                    <button
+                        className="transition duration-500 w-[15vw] text-xl p-3 border-2 rounded-full hover:bg-white hover:text-black">
+                        Pausar contest
+                    </button>
+                    <button
+                        className="transition duration-500 w-[15vw] text-xl p-3 border-2 rounded-full hover:bg-white hover:text-black">
+                        Ir al revelator
+                    </button>
                 </div>
                 <div className="flex items-center justify-center">
-                    <button>Agregar veredicto</button>
+                    <button
+                        className="transition duration-500 w-[15vw] h-[12vh] text-xl p-3 border-2 rounded-full hover:bg-[#2596be] hover:text-white flex items-center justify-center">
+                        <PlusIcon className="w-6 h-6 mr-2" />
+                        Agregar veredicto
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/scoreboard/Timer.tsx
+++ src/components/scoreboard/Timer.tsx
@@ -1,7 +1,38 @@
-import { ReactElement } from "react";
+import { useTimer } from 'react-timer-hook';
 
-const Timer = (): ReactElement => {
+interface MyTimerProps {
+    expiryTimestamp: Date;
+}
 
+const Timer: React.FC<MyTimerProps> = ({expiryTimestamp})=> {
+    const {
+        seconds,
+        minutes,
+        hours,
+        isRunning,
+        start,
+        pause,
+        resume,
+        restart,
+    } = useTimer({ expiryTimestamp, onExpire: () => alert('El contest ha finalizado') });
+    return (
+        <div className="flex flex-col items-center justify-center w-[30vw] h-[35vh] text-center border-2 border-white rounded-lg p-10">
+            <p>Tiempo restante:</p>
+            <div style={{fontSize: '100px'}}>
+                <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+            </div>
+            {/* <p>{isRunning ? 'Running' : 'Not running'}</p>
+            <button onClick={start}>Start</button>
+            <button onClick={pause}>Pause</button>
+            <button onClick={resume}>Resume</button>
+            <button onClick={() => {
+                // Restarts to 5 minutes timer
+                const time = new Date();
+                time.setSeconds(time.getSeconds() + 300);
+                restart(time)
+            }}>Restart</button> */}
+        </div>
+    )
 }
 
 export default Timer;
