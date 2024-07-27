cc7fd2c2f5e0972b47f53fdffb41289ad772f590
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-07-26 23:19:14 -0500

    Timer styles changed

--- src/components/scoreboard/Timer.tsx
+++ src/components/scoreboard/Timer.tsx
@@ -4,9 +4,9 @@ const Timer: React.FC = ()=> {
     const { seconds, minutes, hours, isRunning } = useSelector((state: any) => state.timer);
 
     return (
-        <div className="flex flex-col items-center justify-center w-[30vw] h-[35vh] text-center border-2 border-white rounded-lg p-10">
-            <p>Tiempo restante:</p>
-            <div style={{fontSize: '100px'}}>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <p className='text-sm'>Tiempo restante:</p>
+            <div>
                 <span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
             </div>
             <p>{isRunning ? 'El tiempo esta corriendo' : ''}</p>
