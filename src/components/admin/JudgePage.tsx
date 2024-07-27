a8f9b127ee1ab8ea6b300f694fec158ded79cf33
Adrian Ruiz <adrianruiz0208@gmail.com>
2024-07-27 13:46:33 -0500

    JudgePage Ok. Pending: Add verdict functionality

--- src/components/admin/JudgePage.tsx
+++ src/components/admin/JudgePage.tsx
@@ -5,7 +5,7 @@ import Timer from "../scoreboard/Timer";
 import { useTimer } from 'react-timer-hook';
 import { useNavigate } from "react-router-dom";
 import { pauseTimer, resetTimer, resumeTimer, setTimer, startTimer } from "../../utils/store/timerSlice";
-
+import Modal from 'react-modal';
 
 const JudgePage = () : ReactElement => {
     const navigate = useNavigate();
@@ -17,12 +17,11 @@ const JudgePage = () : ReactElement => {
     const expiryTimestamp = new Date();
     expiryTimestamp.setSeconds(expiryTimestamp.getSeconds() + contestData.durationMinutes * 60); 
     
+    //Estados del componente
     const [initialStateTimer, setInitialStateTimer] = useState(true);
 
-    // Obtenemos el estado del contest
+    const [modalProblemIsOpen, setModalProblemIsOpen] = useState(false);
 
-    const expiry = new Date();
-    expiry.setSeconds(expiry.getSeconds() + contestData.durationMinutes * 60);
 
     const {
         seconds: timerSeconds,
@@ -51,6 +50,7 @@ const JudgePage = () : ReactElement => {
         dispatch(setTimer({ seconds: timerSeconds, minutes: timerMinutes, hours: timerHours }));
     }, [timerSeconds, timerMinutes, timerHours, dispatch]);
     
+    // -- TIMER --
     const handleStart = () => {
         dispatch(startTimer());
         start();
@@ -74,6 +74,33 @@ const JudgePage = () : ReactElement => {
         restart(timeReseted, false);
         setInitialStateTimer(true);
     };
+
+    // --
+    // -- MODAL --
+    const openModalProblem = () => {
+        setModalProblemIsOpen(true);
+    };
+    const afterOpenModal = () => {
+        // setProblemLetter("");
+        // setProblemName("");
+        // setErrorMessageProblem("");
+    };
+    const closeModalAdd = () => {
+        // if (!validateProblemLetter(problemLetter)) {
+        //     setErrorMessageProblem(`Wrong letter, expected letter: ${String.fromCharCode("A".charCodeAt(0) + problems.length)}`);
+        //     return;
+        // } else if (problemName.length <= 0) {
+        //     setErrorMessageProblem("Problem name is required");
+        //     return;
+        // } else {
+        //     handleAddProblem();
+        //     setModalProblemIsOpen(false);
+        // }
+    };
+    const closeModalWithoutAddProblem = () => {
+        setModalProblemIsOpen(false);
+    };
+    // -- 
 
     return (
         <div className="text-white flex items-center justify-center h-[100vh] w-full px-20">
@@ -109,12 +136,67 @@ const JudgePage = () : ReactElement => {
                 </div>
                 <div className="flex items-center justify-center">
                     <button
+                        onClick={openModalProblem}
                         className="transition duration-500 w-[15vw] h-[12vh] text-xl p-3 border-2 rounded-full hover:bg-[#2596be] hover:text-white flex items-center justify-center">
                         <PlusIcon className="w-6 h-6 mr-2" />
                         Agregar veredicto
                     </button>
                 </div>
             </div>
+            <Modal
+                isOpen={modalProblemIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModalAdd}
+                contentLabel="Add Verdict Modal"
+                className="w-[45%] md:w-[45%] lg:w-[25%] h-[45%] p-4 mx-auto my-8 bg-black border-4 border-white rounded-lg flex flex-col justify-center items-center"
+                overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+            >
+                <form className="flex flex-col items-center mt-4 w-full">
+                    <h2 className="text-2xl mb-2 text-white">Agregue su veredicto.</h2>
+                    {/* Team */}
+                    <select
+                        className="p-2 m-2 w-[85%] bg-gray-200 text-black border border-gray-400 rounded"
+                        required
+                    >
+                        <option value="" disabled selected>Select a team</option>
+                    </select>
+                    {/* Problem */}
+                    <select
+                        className="p-2 m-2 w-[85%] bg-gray-200 text-black border border-gray-400 rounded"
+                        required
+                    >
+                        <option value="" disabled selected>Select a problem</option>
+                    </select>
+                    {/* Verdict */}
+                    <select
+                        className="p-2 m-2 w-[85%] bg-gray-200 text-black border border-gray-400 rounded"
+                        required
+                    >
+                        <option value="" disabled selected>Select a verdict</option>
+                        <option value="CORRECT">Correct</option>
+                        <option value="WA">Wrong Answer</option>
+                        <option value="TLE">Time Limit Exceeded</option>
+                    </select>
+                    {/* {errorMessageProblem && (
+                        <p className="text-red-300">{errorMessageProblem}</p>
+                    )} */}
+                    <div className="flex space-x-2 w-full justify-center mt-8">
+                        <button 
+                            type="submit"
+                            onClick={closeModalAdd}
+                            className="p-2 w-[40%] bg-black border-2 border-white rounded-2xl text-white mt-4"
+                        >
+                            Add Problem
+                        </button>
+                        <button
+                            onClick={closeModalWithoutAddProblem}
+                            className="p-2 w-[40%] bg-black border-2 border-white rounded-2xl text-white mt-4"
+                        >
+                            Back
+                        </button>
+                    </div>
+                </form>
+            </Modal>
         </div>
     );
 }
